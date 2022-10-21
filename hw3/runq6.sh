a1ab33d2fe01d37c0e6fbc11f7837695a5d9efb6
Harvey Hu <yubinhu@126.com>
2022-10-21 11:02:48 -0700

    SAC Perfected

--- hw3/runq6.sh
+++ hw3/runq6.sh
@@ -2,5 +2,5 @@ nohup python cs285/scripts/run_hw3_sac.py \
 --env_name HalfCheetah-v4 --ep_len 150 \
 --discount 0.99 --scalar_log_freq 1500 \
 -n 2000000 -l 2 -s 256 -b 1500 -eb 1500 \
--lr 0.0003 --init_temperature 0.1 --exp_name q6b_sac_HalfCheetah_actorfreq10_fixed \
---seed 5 --actor_update_frequency 10
+-lr 0.0003 --init_temperature 0.1 --exp_name q6b_sac_HalfCheetah_actorfreq10_fixed2 \
+--seed 6 --actor_update_frequency 10
