727e885cdda0baedd6001072ac89109f955b3d7f
Harvey Hu <yubinhu@126.com>
2022-11-03 13:04:29 -0700

    hw4q5q6 run finishes

--- hw4/q6.sh
+++ hw4/q6.sh
@@ -15,3 +15,9 @@ nohup python cs285/scripts/run_hw4_mbpo.py --exp_name q6_cheetah_rlen10 --env_na
 --batch_size 5000 --n_iter 10 --video_log_freq -1 --sac_discount 0.99 \
 --sac_n_layers 2 --sac_size 256 --sac_batch_size 1500 --sac_learning_rate 0.0003 \
 --sac_init_temperature 0.1 --sac_n_iter 5000 --mbpo_rollout_length 10
+
+nohup python cs285/scripts/run_hw4_mb.py --exp_name q5_cheetah_random --env_name 'cheetah-cs285-v0' --mpc_horizon 15 --add_sl_noise --num_agent_train_steps_per_iter 1500 --batch_size_initial 5000 --batch_size 5000 --n_iter 5 --video_log_freq -1 --mpc_action_sampling_strategy 'random'
+
+nohup python cs285/scripts/run_hw4_mb.py --exp_name q5_cheetah_cem_2 --env_name 'cheetah-cs285-v0' --mpc_horizon 15 --add_sl_noise --num_agent_train_steps_per_iter 1500 --batch_size_initial 5000 --batch_size 5000 --n_iter 5 --video_log_freq -1 --mpc_action_sampling_strategy 'cem' --cem_iterations 2
+
+nohup python cs285/scripts/run_hw4_mb.py --exp_name q5_cheetah_cem_4 --env_name 'cheetah-cs285-v0' --mpc_horizon 15 --add_sl_noise --num_agent_train_steps_per_iter 1500 --batch_size_initial 5000 --batch_size 5000 --n_iter 5 --video_log_freq -1 --mpc_action_sampling_strategy 'cem' --cem_iterations 4
